04976f6be6e354bd9c747633d9d194c22b2fd5af
nothariharan <nothariharan@gmail.com>
2025-10-26 14:35:49 +0530

    added a /wipe to the main project to delete existing data from the website

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
 import {type RouteConfig, index, route} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"),route('/auth','routes/auth.tsx'),route('/upload','routes/upload.tsx'),route('/content/:id','routes/content.tsx'),] satisfies RouteConfig;
+export default [index("routes/home.tsx"),route('/auth','routes/auth.tsx'),route('/upload','routes/upload.tsx'),route('/content/:id','routes/content.tsx'),route('/wipe','routes/wipe.tsx')] satisfies RouteConfig;
